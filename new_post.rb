0ebc49f133bf5ecf92338611a36a5cd0876460b0
Pooria <pjrashidi@gmail.com>
2023-07-09 19:15:43 -0500

    WIP

---
+++ new_post.rb
@@ -0,0 +1,20 @@
+require 'time'
+
+title = ARGV[0]
+date = Time.now.strftime("%Y-%m-%d")
+filename = "#{date}-#{title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}.md"
+path = File.join("src/_posts", filename)
+
+if File.exist?(path)
+  abort("A post already exists at #{path}")
+else
+  File.open(path, 'w') do |file|
+    file.puts("---")
+    file.puts("title: \"#{title}\"")
+    file.puts("date: #{Time.now}")
+    file.puts("---")
+  end
+end
+
+puts "Created new post at #{path}"
+
